1053eea515eda552c8e57f4644cd511a2ab85a47
Fabio Martignoni <fabio.martignoni@surevive.ch>
2025-07-03 18:52:15 +0200

    fix broken Package

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/FabioMartignoni89/maps-core", .exact("3.3.5")),
+        .package(url: "https://github.com/FabioMartignoni89/maps-core.git", .exact("3.3.5")),
         .package(url: "https://github.com/openmobilemaps/layer-gps.git", .exact("3.3.3"))
     ],
     targets: [
